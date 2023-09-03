d176e01180244affd40d463704631a5c86589d08
Adson Martins <adson.martins982@gmail.com>
2023-09-03 14:16:44 -0300

    feat: improved use fetch hook

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,11 +1,9 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
 export const useFetch = async (url: string, options?: RequestInit) => {
   const headers = {
     ...options?.headers,
   };
 
-  return fetch(`${process.env.API_ENDPOINT}${url}`, {
+  return fetch(`${process.env.API_ENDPOINT || "/api/"}${url}`, {
     ...options,
     headers,
   });
