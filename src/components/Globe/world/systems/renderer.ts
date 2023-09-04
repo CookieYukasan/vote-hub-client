95036d7256df0ad4b92f2207b87c166cb93169fa
Adson Martins <adson.martins982@gmail.com>
2023-09-04 19:02:42 -0300

    feat: improved globe performance

--- src/components/Globe/world/systems/renderer.ts
+++ src/components/Globe/world/systems/renderer.ts
@@ -4,7 +4,7 @@ import { canvasHeight, canvasWidth } from "./config";
 function createRenderer() {
   const renderer = new WebGLRenderer({ antialias: false, alpha: true });
 
-  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setPixelRatio(window.devicePixelRatio * 0.5);
   renderer.setSize(canvasWidth(), canvasHeight());
   renderer.setClearColor(0x000000, 0);
 
